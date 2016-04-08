bb88df6871adc8ea69d34613eeea982b36e85f90
bonecrushereb <bonecrushereb@gmail.com>
2016-04-08 09:31:49 -0700

    finished nodBeginner book

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname +' recieved.');
-
     route(handle, pathname, response, request);
   }
 
